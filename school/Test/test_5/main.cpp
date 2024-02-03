c6a4ef232e9f0389da3d13dcad02f5c0d01f1708
YOONSOMI <103096811+YoonSoM@users.noreply.github.com>
2024-02-03 22:11:34 +0900

    Add files via upload

---
+++ school/Test/test_5/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n1, n2, n3;
+	cout << "n1의 값을 입력하세요";
+	cin >> n1;
+	n2 = n1;
+	n3 = n1;
+	cout << "n1의 값은 " << n1 << "입니다." << endl;
+	cout << "n2의 값은 " << n2 << "입니다." << endl;
+	cout << "n3의 값은 " << n3 << "입니다." << endl;
+	return 0;
+}
