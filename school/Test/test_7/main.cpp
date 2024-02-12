1e15b94eaf26d55fa19db9db4acf931266017eed
YOONSOMI <103096811+YoonSoM@users.noreply.github.com>
2024-02-12 18:21:08 +0900

    Add files via upload

---
+++ school/Test/test_7/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n1 = 10;
+	int n2 = 20;
+	if (n1 < n2) {
+		cout << "n1은 n2보다 작습니다" << endl;
+		cout << "n1은 n2보다 작거나 같습니다" << endl;
+	}
+	return 0;
+}
