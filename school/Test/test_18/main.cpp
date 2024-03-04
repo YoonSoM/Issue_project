67f16638a388aed8dc5f57a93cbebf7450a8b447
YOONSOMI <103096811+YoonSoM@users.noreply.github.com>
2024-03-04 22:58:52 +0900

    Add files via upload

---
+++ school/Test/test_18/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			cout << "¾È³çÇÏ¼¼¿ä" << endl;
+		}
+		cout << endl;
+	}
+	return 0;
+}
