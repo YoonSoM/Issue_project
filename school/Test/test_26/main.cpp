cb5669b1041193c10a2e084d5f1895443e346b74
YOONSOMI <103096811+YoonSoM@users.noreply.github.com>
2024-03-08 13:02:15 +0900

    Add files via upload

---
+++ school/Test/test_26/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	/*
+	for (int i = 1; i <= 100; i++) {
+	if (i % 5 == 0)
+	cout << i << endl;
+	}*/
+	for (int i = 5; i <= 100; i += 5)
+		cout << i << endl;
+	return 0;
+}
