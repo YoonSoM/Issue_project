e368f6e126e6bd3a5f4c2f5d606fccc4f2f646ba
YOONSOMI <103096811+YoonSoM@users.noreply.github.com>
2024-03-11 17:23:15 +0900

    Add files via upload

---
+++ school/Test/test_32/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int i = 0;
+	cout << "프로그램을 시작합니다" << endl;
+	do {
+		cout << "안녕하세요" << endl;
+		i++;
+	} while (i < 10);
+	cout << "프로그램을 종료합니다" << endl;
+	return 0;
+}
