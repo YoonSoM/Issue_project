b7f9f340f043c891ab0971e1a4f99a595b8d2983
YOONSOMI <103096811+YoonSoM@users.noreply.github.com>
2024-03-17 03:34:54 +0900

    Add files via upload

---
+++ school/Test/test_42/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a = 10;
+	cout << "a의 값은 " << a << "입니다" << endl;
+	a = a / 2;
+	cout << "a를 2로 나눈 값은 " << a << "입니다" << endl;
+	int b = 4;
+	cout << "b의 값은 " << b << "입니다" << endl;
+	b = b / 2;
+	cout << "b를 2로 나눈 값은 " << b << "입니다" << endl;
+	int c = 8;
+	cout << "c의 값은 " << c << "입니다" << endl;
+	c = c / 2;
+	cout << "c를 2로 나눈 값은 " << c << "입니다" << endl;
+	return 0;
+}
