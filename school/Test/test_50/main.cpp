86bdace3e74624326b98eefd93eb8cf4df59acc3
YOONSOMI <103096811+YoonSoM@users.noreply.github.com>
2024-03-21 23:02:53 +0900

    Add files via upload

---
+++ school/Test/test_50/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a[5];
+	int result;
+	a[0] = 1;
+	a[1] = 2;
+	a[2] = 3;
+	a[3] = 4;
+	a[4] = 5;
+	cout << "a[0]은 " << a[0] << "입니다" << endl;
+	cout << "a[1]은 " << a[1] << "입니다" << endl;
+	cout << "a[2]은 " << a[2] << "입니다" << endl;
+	cout << "a[3]은 " << a[3] << "입니다" << endl;
+	cout << "a[4]은 " << a[4] << "입니다" << endl;
+	result = a[0] + a[2];
+	cout << "result는 " << result << "입니다" << endl;
+	result = a[0] - a[2];
+	cout << "result는 " << result << "입니다" << endl;
+	result = a[0] * a[2];
+	cout << "result는 " << result << "입니다" << endl;
+	result = a[2] / a[0];
+	cout << "result는 " << result << "입니다" << endl;
+	return 0;
+}
