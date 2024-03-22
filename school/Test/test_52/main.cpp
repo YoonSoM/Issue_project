c8876bfddefb559141cfaed74e2ca2800695110b
YOONSOMI <103096811+YoonSoM@users.noreply.github.com>
2024-03-22 21:03:02 +0900

    Add files via upload

---
+++ school/Test/test_52/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a[5];
+	a[0] = 1;
+	a[1] = 2;
+	a[2] = 3;
+	a[3] = 4;
+	a[4] = 5;
+	cout << a[0] << a[1] << a[2] << a[3] << a[4] << endl;
+	cout << a[5];
+	return 0;
+}
