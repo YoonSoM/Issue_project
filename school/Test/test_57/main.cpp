b77ab87585ceec6ee6d7882b7b203e29c6405bfd
YOONSOMI <103096811+YoonSoM@users.noreply.github.com>
2024-03-24 21:16:24 +0900

    Add files via upload

---
+++ school/Test/test_57/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	//double a[3] = {1.2, 4, 3.4};
+	float a[3] = { 1.2, 4, 3.4 };
+	cout << "a[0]은 " << a[0] << "입니다" << endl;
+	cout << "a[1]은 " << a[1] << "입니다" << endl;
+	cout << "a[2]은 " << a[2] << "입니다" << endl;
+	return 0;
+}
