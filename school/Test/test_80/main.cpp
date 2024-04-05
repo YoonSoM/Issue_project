565549ebea4e33fa318915fc7746af8ca4c63a29
YOONSOMI <103096811+YoonSoM@users.noreply.github.com>
2024-04-06 01:16:30 +0900

    Add files via upload

---
+++ school/Test/test_80/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a = 10;
+	int* b = &a;
+	cout << "a의 주소값은 " << &a << endl;
+	cout << "a의 주소값은 " << b << endl;
+	cout << "a의 값은 " << a << endl;
+	cout << "a의 값은 " << *b << endl;
+	return 0;
+}
